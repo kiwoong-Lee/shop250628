a68867fd38069bb3fe188a8665c9c39d6ba2379f
kiwoong-Lee <54901030+kiwoong-Lee@users.noreply.github.com>
2025-06-28 17:13:53 +0900

    commit

---
+++ inventory/src/main/java/shop/domain/Inventory.java
@@ -0,0 +1,95 @@
+package shop.domain;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import javax.persistence.*;
+import lombok.Data;
+import shop.InventoryApplication;
+import shop.domain.StockDecreased;
+import shop.domain.StockIncreased;
+
+@Entity
+@Table(name = "Inventory_table")
+@Data
+//<<< DDD / Aggregate Root
+public class Inventory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    private Integer stock;
+
+    public static InventoryRepository repository() {
+        InventoryRepository inventoryRepository = InventoryApplication.applicationContext.getBean(
+            InventoryRepository.class
+        );
+        return inventoryRepository;
+    }
+
+    //<<< Clean Arch / Port Method
+    public static void decreasedStock(DeliveryStarted deliveryStarted) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Inventory inventory = new Inventory();
+        repository().save(inventory);
+
+        StockDecreased stockDecreased = new StockDecreased(inventory);
+        stockDecreased.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+
+        repository().findById(deliveryStarted.get???()).ifPresent(inventory->{
+            
+            inventory // do something
+            repository().save(inventory);
+
+            StockDecreased stockDecreased = new StockDecreased(inventory);
+            stockDecreased.publishAfterCommit();
+
+         });
+        */
+
+    }
+
+    //>>> Clean Arch / Port Method
+    //<<< Clean Arch / Port Method
+    public static void increasedStock(DeliveryCanceled deliveryCanceled) {
+        //implement business logic here:
+
+        /** Example 1:  new item 
+        Inventory inventory = new Inventory();
+        repository().save(inventory);
+
+        StockIncreased stockIncreased = new StockIncreased(inventory);
+        stockIncreased.publishAfterCommit();
+        */
+
+        /** Example 2:  finding and process
+        
+
+        repository().findById(deliveryCanceled.get???()).ifPresent(inventory->{
+            
+            inventory // do something
+            repository().save(inventory);
+
+            StockIncreased stockIncreased = new StockIncreased(inventory);
+            stockIncreased.publishAfterCommit();
+
+         });
+        */
+
+    }
+    //>>> Clean Arch / Port Method
+
+}
+//>>> DDD / Aggregate Root
---
+++ inventory/src/main/java/shop/infra/PolicyHandler.java
@@ -0,0 +1,58 @@
+package shop.infra;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.naming.NameParser;
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import shop.config.kafka.KafkaProcessor;
+import shop.domain.*;
+
+//<<< Clean Arch / Inbound Adaptor
+@Service
+@Transactional
+public class PolicyHandler {
+
+    @Autowired
+    InventoryRepository inventoryRepository;
+
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString) {}
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='DeliveryStarted'"
+    )
+    public void wheneverDeliveryStarted_DecreasedStock(
+        @Payload DeliveryStarted deliveryStarted
+    ) {
+        DeliveryStarted event = deliveryStarted;
+        System.out.println(
+            "\n\n##### listener DecreasedStock : " + deliveryStarted + "\n\n"
+        );
+
+        // Sample Logic //
+        Inventory.decreasedStock(event);
+    }
+
+    @StreamListener(
+        value = KafkaProcessor.INPUT,
+        condition = "headers['type']=='DeliveryCanceled'"
+    )
+    public void wheneverDeliveryCanceled_IncreasedStock(
+        @Payload DeliveryCanceled deliveryCanceled
+    ) {
+        DeliveryCanceled event = deliveryCanceled;
+        System.out.println(
+            "\n\n##### listener IncreasedStock : " + deliveryCanceled + "\n\n"
+        );
+
+        // Sample Logic //
+        Inventory.increasedStock(event);
+    }
+}
+//>>> Clean Arch / Inbound Adaptor
